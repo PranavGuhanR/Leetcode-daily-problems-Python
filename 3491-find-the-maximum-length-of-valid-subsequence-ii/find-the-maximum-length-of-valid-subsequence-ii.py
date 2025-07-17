138259a121212e8c201253268efc8cc007370eff
Pranav Guhan R <105731963+PranavGuhanR@users.noreply.github.com>
2025-07-17 22:10:27 +0530

    Update find-the-maximum-length-of-valid-subsequence-ii.py

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.py
@@ -3,7 +3,11 @@ class Solution:
         dp=[]
         m=0
         for i in range(k):
-            dp+=[[0 for j in range(k)]]
+            dp+=[[0 for j in range(k)]]   #dp[j][cn]- for any particular i, 
+                                          #length of the longest subsequence 
+                                          #possible out of nums[0:i+1] ending 
+                                          #with a number cn as nums[i]%k 
+                                          #and sum of adjacent numbers % k as j  
         for i in range(len(nums)):
             cn=nums[i]%k
             for j in range(k):
