17643321aecdeda878e7a8a65212aca1297739d3
Pranav Guhan R <105731963+PranavGuhanR@users.noreply.github.com>
2025-09-12 23:47:05 +0530

    Time: 39 ms (36.36%) | Memory: 18.2 MB (50.30%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        c=0
+        for i in s:       
+            if i in 'aeiou':
+                c+=1
+        if c==0:
+            return False
+        return True             
