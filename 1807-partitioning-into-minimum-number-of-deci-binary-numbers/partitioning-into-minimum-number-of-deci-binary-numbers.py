9c7f6b58741905cf1630bd176dd1ab7b08c6086a
Pranav Guhan R <105731963+PranavGuhanR@users.noreply.github.com>
2026-03-01 16:41:45 +0530

    Time: 59 ms (36.71%) | Memory: 19.9 MB (87.89%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,7 +1,10 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        m=1
-        for i in n:
-            if int(i)>m:
-                m=int(i)
-        return m        
+        max_n = 0
+        for c in n:
+            n = int(c)
+            if n>max_n:
+                max_n = n
+            if max_n==9:
+                return 9
+        return max_n
