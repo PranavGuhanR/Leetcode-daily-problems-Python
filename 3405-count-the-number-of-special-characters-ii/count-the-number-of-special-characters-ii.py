e6dc7b621177c5201c1672223ef3d058bac0f606
Pranav Guhan R <105731963+PranavGuhanR@users.noreply.github.com>
2026-05-27 15:01:25 +0530

    Time: 64 ms (94.12%) | Memory: 21.5 MB (62.94%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -1,16 +1,10 @@
 class Solution:
-    def numberOfSpecialChars(self, word: str) -> int:
-        uc=set()
-        lc=set()
-        d=dict()
-        for i in word:
-            if i>='a':
-                if chr(ord(i)-ord('a')+ord('A')) in uc:
-                    d[i]=0
-                lc.add(i)    
-            else:
-                if chr(ord(i)-ord('A')+ord('a')) in lc:
-                    if chr(ord(i)-ord('A')+ord('a')) not in d:
-                        d[chr(ord(i)-ord('A')+ord('a'))]=1
-                uc.add(i)        
-        return sum(d.values())      
+    def numberOfSpecialChars(self, word: str, ans = 0) -> int:
+        
+        for ch, CH in zip(ascii_lowercase, ascii_uppercase):
+
+            if ch not in word or CH not in word: continue
+  
+            ans+= word.rfind(ch) < word.find(CH)
+
+        return ans
