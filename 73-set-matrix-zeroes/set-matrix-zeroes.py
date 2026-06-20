f9ed0e514d23d282b9218a0ee73ceac028790350
Pranav Guhan R <105731963+PranavGuhanR@users.noreply.github.com>
2026-06-20 18:43:09 +0530

    Time: 11 ms (27.17%) | Memory: 20.3 MB (36.29%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    matrix[i][j]=2**31
+        for i in range(len(matrix)):
+            f=0
+            for j in range(len(matrix[0])): 
+                if matrix[i][j]==2**31:
+                    f=1
+                    break
+            if f:        
+                for j in range(len(matrix[0])):
+                    if matrix[i][j]!=2**31:
+                        matrix[i][j]=0
+        for j in range(len(matrix[0])):
+            f=0
+            for i in range(len(matrix)): 
+                if matrix[i][j]==2**31:
+                    f=1
+                    break
+            if f:        
+                for i in range(len(matrix)):
+                    if matrix[i][j]!=2**31:
+                        matrix[i][j]=0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==2**31:
+                    matrix[i][j]=0
+        return matrix                                         
